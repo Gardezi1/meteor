fa96dc6af7e694b582cf1ff156be83c935f290a9
Gardezi1 <ali.gardezi@kwanso.com>
2015-11-03 21:47:50 +0500

    second commit

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -1,9 +1,7 @@
 Template.postItem.helpers({
 	domain: function(){
-		// console.log("in");
 		var a = document.createElement('a');
 		a.href = this.url;
-		// console.log(a.hostname);
 		return a.hostname;
 	}
 });
